6c6059cd3472434c0d39e392abf11f05cf9f7eaf
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-12-27 16:52:06 -0700

    Reorganize tests.
    Switch to multiple individual tests
    Move tests inside the package

---
+++ ttide/tests/print_tests.py
@@ -0,0 +1,52 @@
+from ..t_tide import t_tide
+from . import base as bmod
+from cStringIO import StringIO
+import sys
+
+
+def compare_string2file(string, fname):
+    with open(bmod.testdir + 'data/print/' + fname, 'r') as fl:
+        tdata = fl.read().replace('\r\n', '\n')
+    string = string.replace('\r\n', '\n')
+    # The above .replace() calls makes sure that line-endings are
+    # consistent
+    assert string == tdata, ("Test failed on file '%s'" % fname)
+
+
+def gen_print_tests(make_data=False):
+    """A generator function for the 'print' tests.
+
+    If make_data=True, this will write out the data files.
+    """
+    out_old = sys.stdout
+    for kwargs, fname in bmod.cases:
+        # Redirect stdout to a StringIO instance
+        sys.stdout = stdout = StringIO()
+        t_tide(**kwargs)
+        sys.stdout = out_old
+        if make_data:
+            with open(bmod.testdir + 'data/print/' + fname, 'w') as fl:
+                fl.write(stdout.getvalue())
+            yield None
+        else:
+            yield compare_string2file, stdout.getvalue(), fname
+
+
+# if __name__ == '__main__':
+
+#     # ###
+#     # # This block generates the output files.
+#     # # USE WITH CAUTION!
+#     # for t in gen_print_tests(make_data=True):
+#     #     pass
+
+#     ###
+#     # This block is for joining files for comparison to the old testdata.out file.
+#     # It is not identical, but nearly so...
+#     outstr = ''
+#     for kwargs, fl in cases:
+#         with open(testdir + 'data/' + fl, 'r') as fl:
+#             outstr += fl.read()
+#             outstr += '\n' + '*' * 80 + '\n\n'
+#     with open(testdir + 'data/testdata.out2', 'w') as fl:
+#         fl.write(outstr)
