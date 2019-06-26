6d27730c8470d6592e518ffcf6a99164c19ed8e9
Oleksandr (Sasha) Huziy <oleksandr.huziy@canada.ca>
2019-06-26 19:02:03 +0000

    start writing tests

--- ttide/tests/tide_tests.py
+++ ttide/tests/tide_tests.py
@@ -9,8 +9,43 @@ def test_errcalc():
     """
     errcalc_opts = ("cboot", "linear", "none")
 
+
+    con_cboot = None
+
+    msg_format = """
+
+    {} should be the same for all errcalc values.
+    But:
+    errcalc={}, yields mean {}={}
+    and
+    errcalc={}, yields mean {}={}
+    """
+
+
     for errcalc in errcalc_opts:
-        tt.t_tide(base.ein, dt=1., synth=0, ray=0.5, out_style=None, errcalc=errcalc)
+        con = tt.t_tide(base.ein, dt=1., synth=0, ray=0.5, out_style=None, errcalc=errcalc)
+
+        print(con["tidecon"])
+
+        if con_cboot is None:
+            con_cboot = con
+            amp_mean_cboot = con_cboot["tidecon"][:, 0].mean()
+            pha_mean_cboot = con_cboot["tidecon"][:, 2].mean()
+        else:
+            # amplitude and phase should not change
+            amp_mean = con["tidecon"][:, 0].mean()
+            pha_mean = con["tidecon"][:, 2].mean()
+
+
+
+
+            param = "amplitude"
+            msg = msg_format.format(param, errcalc, param, amp_mean, errcalc_opts[0], param, amp_mean_cboot)
+            assert amp_mean == amp_mean_cboot, msg
+
+            param = "phase"
+            msg = msg_format.format(param, errcalc, param, pha_mean, errcalc_opts[0], param, pha_mean_cboot)
+            assert pha_mean == pha_mean_cboot, msg
 
 
 def test_all():
